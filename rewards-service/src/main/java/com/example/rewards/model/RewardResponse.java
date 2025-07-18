2803500735aa2c680c9676dbc3c65901373cd7e8
ascendion-linesh <linesh.mishra@ascendion.com>
2025-07-18 18:52:32 +0530

    Add or update file: RewardResponse.java

--- rewards-service/src/main/java/com/example/rewards/model/RewardResponse.java
+++ rewards-service/src/main/java/com/example/rewards/model/RewardResponse.java
@@ -3,44 +3,26 @@ package com.example.rewards.model;
 import java.math.BigDecimal;
 import java.util.List;
 
-/**
- * Represents the response containing discounts and rewards for a cart evaluation.
- */
 public class RewardResponse {
-    private BigDecimal totalDiscount;
-    private List<DiscountDetail> discounts;
-    private List<LoyaltyReward> loyaltyRewards;
+    private BigDecimal discount;
+    private List<String> appliedCampaigns;
+    private List<String> loyaltyActions;
+    private String message;
 
-    public BigDecimal getTotalDiscount() { return totalDiscount; }
-    public void setTotalDiscount(BigDecimal totalDiscount) { this.totalDiscount = totalDiscount; }
-    public List<DiscountDetail> getDiscounts() { return discounts; }
-    public void setDiscounts(List<DiscountDetail> discounts) { this.discounts = discounts; }
-    public List<LoyaltyReward> getLoyaltyRewards() { return loyaltyRewards; }
-    public void setLoyaltyRewards(List<LoyaltyReward> loyaltyRewards) { this.loyaltyRewards = loyaltyRewards; }
-
-    public static class DiscountDetail {
-        private String campaignName;
-        private String description;
-        private BigDecimal amount;
-
-        public String getCampaignName() { return campaignName; }
-        public void setCampaignName(String campaignName) { this.campaignName = campaignName; }
-        public String getDescription() { return description; }
-        public void setDescription(String description) { this.description = description; }
-        public BigDecimal getAmount() { return amount; }
-        public void setAmount(BigDecimal amount) { this.amount = amount; }
+    public RewardResponse() {}
+    public RewardResponse(BigDecimal discount, List<String> appliedCampaigns, List<String> loyaltyActions, String message) {
+        this.discount = discount;
+        this.appliedCampaigns = appliedCampaigns;
+        this.loyaltyActions = loyaltyActions;
+        this.message = message;
     }
 
-    public static class LoyaltyReward {
-        private String program;
-        private int points;
-        private String rewardType;
-
-        public String getProgram() { return program; }
-        public void setProgram(String program) { this.program = program; }
-        public int getPoints() { return points; }
-        public void setPoints(int points) { this.points = points; }
-        public String getRewardType() { return rewardType; }
-        public void setRewardType(String rewardType) { this.rewardType = rewardType; }
-    }
+    public BigDecimal getDiscount() { return discount; }
+    public void setDiscount(BigDecimal discount) { this.discount = discount; }
+    public List<String> getAppliedCampaigns() { return appliedCampaigns; }
+    public void setAppliedCampaigns(List<String> appliedCampaigns) { this.appliedCampaigns = appliedCampaigns; }
+    public List<String> getLoyaltyActions() { return loyaltyActions; }
+    public void setLoyaltyActions(List<String> loyaltyActions) { this.loyaltyActions = loyaltyActions; }
+    public String getMessage() { return message; }
+    public void setMessage(String message) { this.message = message; }
 }
