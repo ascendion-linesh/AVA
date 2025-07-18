40947e1d56d6a87bb303fb52c97611f96ff9288c
ascendion-linesh <linesh.mishra@ascendion.com>
2025-07-18 18:49:39 +0530

    Add or update file: JsonUtils.java

---
+++ order-service/src/main/java/com/ascendion/ava/orderservice/util/JsonUtils.java
@@ -0,0 +1,16 @@
+package com.ascendion.ava.orderservice.util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JsonUtils {
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    public static String toJson(Object obj) {
+        try {
+            return objectMapper.writeValueAsString(obj);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Failed to serialize object to JSON", e);
+        }
+    }
+}
