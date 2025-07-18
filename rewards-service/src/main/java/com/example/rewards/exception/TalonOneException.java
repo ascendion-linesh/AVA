93986a68507a7b2e0935b00f74dd38c3322f4e4e
ascendion-linesh <linesh.mishra@ascendion.com>
2025-07-18 18:52:38 +0530

    Add or update file: TalonOneException.java

--- rewards-service/src/main/java/com/example/rewards/exception/TalonOneException.java
+++ rewards-service/src/main/java/com/example/rewards/exception/TalonOneException.java
@@ -1,13 +1,9 @@
 package com.example.rewards.exception;
 
-/**
- * Custom exception for Talon.One API integration errors.
- */
 public class TalonOneException extends RuntimeException {
     public TalonOneException(String message) {
         super(message);
     }
-
     public TalonOneException(String message, Throwable cause) {
         super(message, cause);
     }
