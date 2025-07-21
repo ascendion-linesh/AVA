2a284aa7fcca778cf91a6a2470020dff291718bc
ascendion-linesh <linesh.mishra@ascendion.com>
2025-07-21 12:15:07 +0530

    Add or update file: User.java

--- src/main/java/com/app/model/User.java
+++ src/main/java/com/app/model/User.java
@@ -1,35 +1,37 @@
 package com.app.model;
 
 import jakarta.persistence.*;
-import lombok.*;
+lombok.Data;
+lombok.NoArgsConstructor;
 
 import java.util.List;
 
 /**
- * Entity representing a user in the e-commerce system.
+ * Entity representing a user in the system.
  */
 @Entity
 @Table(name = "users")
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
-@Builder
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
+    private String name;
+
     @Column(nullable = false, unique = true)
     private String email;
 
-    private int totalOrders;
+    @Column(nullable = false)
+    private Integer totalOrders = 0;
 
-    private double totalSpent;
+    @Column(nullable = false)
+    private Double totalSpent = 0.0;
 
     // One user can have many orders
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
-    @ToString.Exclude
-    @EqualsAndHashCode.Exclude
     private List<Order> orders;
 }
