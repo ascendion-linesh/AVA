6982b457535114fd3c0cfbb3ce8fd1d6a91d3594
ascendion-linesh <linesh.mishra@ascendion.com>
2025-07-21 10:01:28 +0530

    Add or update file: TalonOneService.java

--- rewards-service/src/main/java/com/example/rewards/service/TalonOneService.java
+++ rewards-service/src/main/java/com/example/rewards/service/TalonOneService.java
@@ -2,16 +2,15 @@ package com.example.rewards.service;
 
 import com.example.rewards.config.TalonOneConfig;
 import com.example.rewards.exception.TalonOneException;
+import com.example.rewards.model.RewardResponse;
 import com.example.rewards.model.TalonOneSessionRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.Map;
 
 @Service
 public class TalonOneService {
@@ -26,24 +25,29 @@ public class TalonOneService {
         this.restTemplate = new RestTemplate();
     }
 
-    public Map<String, Object> evaluateSession(TalonOneSessionRequest request) {
-        String url = talonOneConfig.getApiUrl() + "/v1/customer_sessions";
+    public RewardResponse evaluateSession(TalonOneSessionRequest request) {
+        String url = talonOneConfig.getBaseUrl() + "/v1/customer_sessions";
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         headers.set("Authorization", "ApiKey-v1 " + talonOneConfig.getApiKey());
+        headers.set("X-Talon-Application", talonOneConfig.getApplicationId());
         HttpEntity<TalonOneSessionRequest> entity = new HttpEntity<>(request, headers);
         try {
-            logger.info("Calling Talon.One API at {}", url);
-            ResponseEntity<Map> response = restTemplate.exchange(url, HttpMethod.POST, entity, Map.class);
-            if (response.getStatusCode().is2xxSuccessful()) {
-                return response.getBody();
-            } else {
-                logger.error("Talon.One API returned status: {}", response.getStatusCode());
-                throw new TalonOneException("Talon.One API error: " + response.getStatusCode());
-            }
-        } catch (RestClientException ex) {
-            logger.error("Talon.One API call failed", ex);
-            throw new TalonOneException("Talon.One API call failed", ex);
+            logger.debug("Sending request to Talon.One: {}", request);
+            ResponseEntity<RewardResponse> response = restTemplate.exchange(
+                    url,
+                    HttpMethod.POST,
+                    entity,
+                    RewardResponse.class
+            );
+            logger.debug("Received response from Talon.One: {}", response.getBody());
+            return response.getBody();
+        } catch (HttpStatusCodeException e) {
+            logger.error("Talon.One API error: {} - {}", e.getStatusCode(), e.getResponseBodyAsString());
+            throw new TalonOneException("Talon.One API error: " + e.getStatusCode() + " - " + e.getResponseBodyAsString(), e);
+        } catch (Exception e) {
+            logger.error("Unexpected error calling Talon.One API", e);
+            throw new TalonOneException("Unexpected error calling Talon.One API", e);
         }
     }
 }
