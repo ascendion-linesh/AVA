79938a480a1f1b116d89e8f5726f76c74e12aa69
ascendion-linesh <linesh.mishra@ascendion.com>
2025-07-21 12:14:22 +0530

    Add or update file: AppApplication.java

--- src/main/java/com/app/AppApplication.java
+++ src/main/java/com/app/AppApplication.java
@@ -3,9 +3,6 @@ package com.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Main entry point for the TalonOneApp Spring Boot application.
- */
 @SpringBootApplication
 public class AppApplication {
     public static void main(String[] args) {
