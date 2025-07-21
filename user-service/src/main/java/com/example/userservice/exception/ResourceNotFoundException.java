6e8a637dc931c1c91782c5a4e0051d3da90476db
ascendion-linesh <linesh.mishra@ascendion.com>
2025-07-21 09:58:41 +0530

    Add or update file: ResourceNotFoundException.java

--- user-service/src/main/java/com/example/userservice/exception/ResourceNotFoundException.java
+++ user-service/src/main/java/com/example/userservice/exception/ResourceNotFoundException.java
@@ -1,8 +1,5 @@
 package com.example.userservice.exception;
 
-/**
- * Exception thrown when a resource is not found.
- */
 public class ResourceNotFoundException extends RuntimeException {
     public ResourceNotFoundException(String message) {
         super(message);
